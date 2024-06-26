431cf7121e81311c6fad7187a261aff7a123eef2
Priscila Tariro Kapuyanyika <Priscilakapuyanyika974@gmail.com>
2024-06-26 09:15:11 +0200

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Weather from "./weather";
+import "./Weather.css";
 
 function App() {
   return (
     <div>
       <Weather />
-      <p>
-        This project was coded by <a href="https://github.com/Tarirohope"></a>
-        Priscila Tariro Kapuyanyika and is{" "}
-        <a href="https://pkapuyanyika.netlify.app/">open sourced</a>
+      <p className="name">
+        This project was coded by{" "}
+        <a href="https://github.com/Tarirohope">Priscila Tariro Kapuyanyika</a>{" "}
+        and is hosted on <a href="https://pkapuyanyika.netlify.app/">Netlify</a>
+        .
       </p>
     </div>
   );
